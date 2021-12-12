5fb760b184c9af6287a00c660d50a8ef1bc8862d
entaromia <entaromia@gmail.com>
2021-12-12 16:17:18 +0300

    touchscreen: sec_ts: partial rewrite
     * Cleanup all samsung/exynos spesific code.
     * Remove unused or unsupported functions.
     * Remove factory and debug testing.
     * Remove fw update related code, we just skip update always.
     * Integrate dt2w driver into main driver.
     * Implement proper suspend/resume.
    
    Signed-off-by: entaromia <entaromia@gmail.com>

--- drivers/input/touchscreen/sec_ts/sec_ts_wake_gestures.c
+++ drivers/input/touchscreen/sec_ts/sec_ts_wake_gestures.c
@@ -20,66 +20,31 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/types.h>
-#include <linux/delay.h>
-#include <linux/init.h>
-#include <linux/err.h>
-#include "include/sec_ts_wake_gestures.h" /* <linux/wake_gestures.h> */
-#include <linux/slab.h>
-#include <linux/workqueue.h>
-#include <linux/input.h>
-#include <linux/hrtimer.h>
-#include <asm-generic/cputime.h>
-
-/* Tuneables */
-#define WG_DEBUG		0
-#define WG_DEFAULT		0
-#define DT2W_DEFAULT		0
-#define WG_PWRKEY_DUR           60
-
-/* GM 9 Pro */
-#define DT2W_FEATHER		150
-#define DT2W_TIME 		300 /* 500 */
-
-/* Wake Gestures */
-#define TRIGGER_TIMEOUT		500
-
-#define LOGTAG			"WG"
+#include "include/sec_ts.h"
 
 /* Resources */
-int dt2w_switch = DT2W_DEFAULT;
+int dt2w_switch;
 int dt2w_switch_temp;
-bool dt2w_switch_changed = false;
-static int touch_x = 0, touch_y = 0;
-static bool touch_x_called = false, touch_y_called = false;
+bool dt2w_switch_changed;
 static bool exec_count = true;
 static unsigned long pwrtrigger_time[2] = {0, 0};
 static unsigned long long tap_time_pre = 0;
 static int touch_nr = 0, x_pre = 0, y_pre = 0;
 static bool touch_cnt = true;
 
-static struct input_dev * wake_dev;
+static struct input_dev * sec_ts_dev;
 static DEFINE_MUTEX(pwrkeyworklock);
-static struct workqueue_struct *dt2w_input_wq;
-static struct work_struct dt2w_input_work;
-
-static bool is_suspended(void)
-{
-	return scr_suspended();
-}
 
 /* PowerKey work func */
 static void wake_presspwr(struct work_struct * wake_presspwr_work) {
 	if (!mutex_trylock(&pwrkeyworklock))
 		return;
 
-	input_event(wake_dev, EV_KEY, KEY_POWER, 1);
-	input_event(wake_dev, EV_SYN, 0, 0);
+	input_event(sec_ts_dev, EV_KEY, KEY_POWER, 1);
+	input_event(sec_ts_dev, EV_SYN, 0, 0);
 	msleep(WG_PWRKEY_DUR);
-	input_event(wake_dev, EV_KEY, KEY_POWER, 0);
-	input_event(wake_dev, EV_SYN, 0, 0);
+	input_event(sec_ts_dev, EV_KEY, KEY_POWER, 0);
+	input_event(sec_ts_dev, EV_SYN, 0, 0);
 	msleep(WG_PWRKEY_DUR);
 	mutex_unlock(&pwrkeyworklock);
 
@@ -97,12 +62,11 @@ static void wake_pwrtrigger(void) {
 
 	schedule_work(&wake_presspwr_work);
 
-        return;
+    return;
 }
 
 
 /* Doubletap2wake */
-
 static void doubletap2wake_reset(void) {
 	exec_count = true;
 	touch_nr = 0;
@@ -128,14 +92,9 @@ static void new_touch(int x, int y) {
 }
 
 /* Doubletap2wake main function */
-static void detect_doubletap2wake(int x, int y, bool st)
+void sec_ts_detect_doubletap2wake(int x, int y)
 {
-        bool single_touch = st;
-#if WG_DEBUG
-        pr_info(LOGTAG"x,y(%4d,%4d) tap_time_pre:%llu\n",
-                x, y, tap_time_pre);
-#endif
-	if ((single_touch) && (dt2w_switch) && (exec_count) && (touch_cnt)) {
+	if (exec_count) {
 		touch_cnt = false;
 		if (touch_nr == 0) {
 			new_touch(x, y);
@@ -161,119 +120,6 @@ static void detect_doubletap2wake(int x, int y, bool st)
 	}
 }
 
-static void dt2w_input_callback(struct work_struct *unused)
-{
-
-	if (is_suspended() && dt2w_switch)
-		detect_doubletap2wake(touch_x, touch_y, true);
-	return;
-}
-
-static void wg_input_event(struct input_handle *handle, unsigned int type,
-				unsigned int code, int value)
-{
-	if (is_suspended() && code == ABS_MT_POSITION_X) {
-		value -= 5000;
-	}
-
-#if WG_DEBUG
-	pr_info("wg: code: %s|%u, val: %i\n",
-		((code==ABS_MT_POSITION_X) ? "X" :
-		(code==ABS_MT_POSITION_Y) ? "Y" :
-		(code==ABS_MT_TRACKING_ID) ? "ID" :
-		"undef"), code, value);
-#endif
-
-	if (code == ABS_MT_SLOT) {
-		doubletap2wake_reset();
-		return;
-	}
-
-	if (code == ABS_MT_TRACKING_ID && value == -1) {
-		touch_cnt = true;
-		queue_work_on(0, dt2w_input_wq, &dt2w_input_work);
-		return;
-	}
-
-	if (code == ABS_MT_POSITION_X) {
-		touch_x = value;
-		touch_x_called = true;
-	}
-
-	if (code == ABS_MT_POSITION_Y) {
-		touch_y = value;
-		touch_y_called = true;
-	}
-
-	if (touch_x_called && touch_y_called) {
-		touch_x_called = false;
-		touch_y_called = false;
-	} else if (!is_suspended() && touch_x_called && !touch_y_called) {
-		touch_x_called = false;
-		touch_y_called = false;
-	}
-}
-
-static int input_dev_filter(struct input_dev *dev) {
-	if (strstr(dev->name, "sec_touchscreen")) {
-		return 0;
-	} else {
-		return 1;
-	}
-}
-
-static int wg_input_connect(struct input_handler *handler,
-				struct input_dev *dev, const struct input_device_id *id) {
-	struct input_handle *handle;
-	int error;
-
-	if (input_dev_filter(dev))
-		return -ENODEV;
-
-	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
-	if (!handle)
-		return -ENOMEM;
-
-	handle->dev = dev;
-	handle->handler = handler;
-	handle->name = "wg";
-
-	error = input_register_handle(handle);
-	if (error)
-		goto err2;
-
-	error = input_open_device(handle);
-	if (error)
-		goto err1;
-
-	return 0;
-err1:
-	input_unregister_handle(handle);
-err2:
-	kfree(handle);
-	return error;
-}
-
-static void wg_input_disconnect(struct input_handle *handle) {
-	input_close_device(handle);
-	input_unregister_handle(handle);
-	kfree(handle);
-}
-
-static const struct input_device_id wg_ids[] = {
-	{ .driver_info = 1 },
-	{ },
-};
-
-static struct input_handler wg_input_handler = {
-	.event		= wg_input_event,
-	.connect	= wg_input_connect,
-	.disconnect	= wg_input_disconnect,
-	.name		= "wg_inputreq",
-	.id_table	= wg_ids,
-};
-
-
 /*
  * SYSFS stuff below here
  */
@@ -294,10 +140,10 @@ static ssize_t doubletap2wake_dump(struct device *dev,
 	if (dt2w_switch_temp < 0 || dt2w_switch_temp > 1)
 		dt2w_switch_temp = 0;
 
-	if (!scr_suspended())
-		dt2w_switch = dt2w_switch_temp;
-	else
+	if (sec_ts_is_suspended())
 		dt2w_switch_changed = true;
+	else
+		dt2w_switch = dt2w_switch_temp;
 
 	return count;
 }
@@ -308,40 +154,18 @@ static DEVICE_ATTR(doubletap2wake, (S_IWUSR|S_IRUGO),
 /*
  * INIT / EXIT stuff below here
  */
-
 struct kobject *android_touch_kobj;
 EXPORT_SYMBOL_GPL(android_touch_kobj);
 
-static int __init wake_gestures_init(void)
+int sec_ts_wake_gestures_init(struct sec_ts_data *ts)
 {
 	int rc = 0;
+	dt2w_switch = 0;
+	dt2w_switch_temp = 0;
+	dt2w_switch_changed = false;
+	sec_ts_dev = ts->input_dev;
 
-	wake_dev = input_allocate_device();
-	if (!wake_dev) {
-		pr_err("Failed to allocate wake_dev\n");
-		goto err_alloc_dev;
-	}
-
-	input_set_capability(wake_dev, EV_KEY, KEY_POWER);
-	wake_dev->name = "wg_pwrkey";
-	wake_dev->phys = "wg_pwrkey/input0";
-
-	rc = input_register_device(wake_dev);
-	if (rc) {
-		pr_err("%s: input_register_device err=%d\n", __func__, rc);
-		goto err_input_dev;
-	}
-
-	rc = input_register_handler(&wg_input_handler);
-	if (rc)
-		pr_err("%s: Failed to register wg_input_handler\n", __func__);
-
-	dt2w_input_wq = create_workqueue("dt2wiwq");
-	if (!dt2w_input_wq) {
-		pr_err("%s: Failed to create dt2wiwq workqueue\n", __func__);
-		return -EFAULT;
-	}
-	INIT_WORK(&dt2w_input_work, dt2w_input_callback);
+	input_set_capability(sec_ts_dev, EV_KEY, KEY_POWER);
 
 	android_touch_kobj = kobject_create_and_add("android_touch", NULL) ;
 	if (android_touch_kobj == NULL) {
@@ -353,24 +177,11 @@ static int __init wake_gestures_init(void)
 		pr_warn("%s: sysfs_create_file failed for doubletap2wake\n", __func__);
 	}
 
-err_input_dev:
-	input_free_device(wake_dev);
-err_alloc_dev:
-
 	return 0;
 }
 
-static void __exit wake_gestures_exit(void)
+int sec_ts_wake_gestures_exit(void)
 {
 	kobject_del(android_touch_kobj);
-	input_unregister_handler(&wg_input_handler);
-	destroy_workqueue(dt2w_input_wq);
-	input_unregister_device(wake_dev);
-	input_free_device(wake_dev);
-
-	return;
+	return 0;
 }
-
-module_init(wake_gestures_init);
-module_exit(wake_gestures_exit);
-
--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -516,11 +516,6 @@
 #define KEY_DEL_EOS		0x1c1
 #define KEY_INS_LINE		0x1c2
 #define KEY_DEL_LINE		0x1c3
-#define KEY_SIDE_GESTURE	0x1c6
-#define KEY_BLACK_UI_GESTURE	0x1c7
-
-#define KEY_SIDE_GESTURE_RIGHT	0x1ca
-#define KEY_SIDE_GESTURE_LEFT	0x1cb
 
 #define KEY_FN			0x1d0
 #define KEY_FN_ESC		0x1d1
@@ -738,9 +733,6 @@
 #define ABS_MT_TOOL_X		0x3c	/* Center X tool position */
 #define ABS_MT_TOOL_Y		0x3d	/* Center Y tool position */
 
-#define ABS_MT_CUSTOM		0x3e	/* custom event */
-#define ABS_MT_GRIP		0x3f    /* grip touch */
-
 #define ABS_MAX			0x3f
 #define ABS_CNT			(ABS_MAX+1)
 
@@ -770,9 +762,6 @@
 #define SW_HPHR_OVERCURRENT	0x10  /* set = over current on right hph */
 #define SW_MICROPHONE2_INSERT   0x11  /* set = inserted */
 #define SW_UNSUPPORT_INSERT	0x12  /* set = unsupported device inserted */
-#define SW_FLIP                 0x15    /* set = flip cover */
-#define SW_CERTIFYHALL          0x1b    /* set = certify_hall... */
-#define SW_MAX_KARI		0x1F
 #define SW_MAX			0x20
 #define SW_CNT			(SW_MAX+1)
 
